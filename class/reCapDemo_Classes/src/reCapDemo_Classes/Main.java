62dc6983eee74de9a0af8306c78375997bf92f46
İbrahim Deniz ÇELİK <ibrahimdenizcelik@gmail.com>
2022-09-26 05:00:14 +0300

    Class Yapıları Hakkında Basit Örnek Çalışması

---
+++ class/reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Operations operations = new Operations();
+		int total = operations.multiplication(3, 4);
+		System.out.println(total);
+	}
+
+}
---
+++ class/reCapDemo_Classes/src/reCapDemo_Classes/Operations.java
@@ -0,0 +1,24 @@
+package reCapDemo_Classes;
+
+public class Operations {
+
+	public int addition(int number1, int number2) {
+		int total = number1 + number2;
+		return total;
+	}
+
+	public int subtraction(int number1, int number2) {
+		int total = number1 - number2;
+		return total;
+	}
+
+	public int division(int number1, int number2) {
+		int total = number1 / number2;
+		return total;
+	}
+
+	public int multiplication(int number1, int number2) {
+		int total = number1 * number2;
+		return total;
+	}
+}
